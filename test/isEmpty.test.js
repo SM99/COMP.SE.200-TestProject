1112643a923e75b1f875df2ccc99da7b16e38c09
hasu <aleksi.hasu@tuni.fi>
2023-12-06 16:24:49 +0200

    fix faulty test

--- test/isEmpty.test.js
+++ test/isEmpty.test.js
@@ -29,7 +29,7 @@ describe('isEmpty function', () => {
     it('Should handle array-like objects', () => {
         expect(isEmpty('string')).toBe(false);
         expect(isEmpty([undefined, null])).toBe(false);
-        expect(isEmpty({ length: 0 })).toBe(true);
+        expect(isEmpty({ length: 0 })).toBe(false);
         expect(isEmpty({ length: 2 })).toBe(false);
     });
 
